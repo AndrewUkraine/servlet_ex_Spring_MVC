a60cf3e2a827d91dcaba691ae8fc01c1500e98b7
AndrewUkraine <andrewukraine17@gmail.com>
2018-02-11 20:04:08 +0200

    added new name of cat

--- src/main/java/entities/Cat.java
+++ src/main/java/entities/Cat.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Cat {
-    private String name = "Триха";
+    private String name = "Триха и Луха";
 
     public String getName() {
         return name;
